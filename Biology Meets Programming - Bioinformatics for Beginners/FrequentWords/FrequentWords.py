408f57bff78703d607d135b056c4ec6728762374
Brayam Andres Saavedra Arce <57733110+Jebrayam@users.noreply.github.com>
2020-11-17 23:09:16 -0500

    Add files via upload

--- Biology Meets Programming - Bioinformatics for Beginners/FrequentWords/FrequentWords.py
+++ Biology Meets Programming - Bioinformatics for Beginners/FrequentWords/FrequentWords.py
@@ -4,9 +4,13 @@ k = 3
 text = "CGATATATCCATAG"
 
 def FrequentWords(text, k):
+    #list to store most common patterns
     words = []
+    #create frequency map with all found patterns
     freq = bl.FrequencyMap(text, k)
+    #find maximun values
     m = max(freq.values())
+    #iterate through patterns to find m repeated ones
     for key in freq:
         if freq[key] == m:
             words.append(key)
