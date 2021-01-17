8b7e54550c21ad30acd3018786171cf8771afc0a
Brayam Andres Saavedra Arce <57733110+Jebrayam@users.noreply.github.com>
2021-01-16 22:21:21 -0500

    Add files via upload

---
+++ Biology Meets Programming - Bioinformatics for Beginners/SymbolArray/FasterSymbolArray.py
@@ -0,0 +1,27 @@
+#faster symbol array
+import biolib as bl
+
+
+def FasterSymbolArray(Genome, symbol):
+    array = {}
+    n = len(Genome)
+    ExtendedGenome = Genome + Genome[0:n//2]
+
+    # look at the first half of Genome to compute first array value
+    array[0] = bl.PatternCount(Genome[0:n//2], symbol)
+
+    for i in range(1, n):
+        # start by setting the current array value equal to the previous array value
+        array[i] = array[i-1]
+
+        # the current array value can differ from the previous array value by at most 1
+        if ExtendedGenome[i-1] == symbol:
+            array[i] = array[i]-1
+        if ExtendedGenome[i+(n//2)-1] == symbol:
+            array[i] = array[i]+1
+    return array
+
+txtFile = open('E_coli.txt')
+genome = txtFile.read()
+symbol = 'A'
+print(FasterSymbolArray(genome, symbol))
